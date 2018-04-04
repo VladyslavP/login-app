8fcba28d7b869e5e55e995a7c4665985e676ca6f
v.posudevskyi <vlapodev@gmail.com>
2018-04-05 02:53:59 +0300

    Task4: Added saving token to localStorage

--- src/sagas/login.js
+++ src/sagas/login.js
@@ -1,30 +1,37 @@
 import { put, call, takeEvery } from 'redux-saga/effects';
 
 import config from '../utils';
-import { getDataAction } from "../common/actions";
+import { postDataAction } from "../common/actions";
+import { setAuthToken } from "../common/helpers";
 import {
     LOGIN_REQUEST_SUCCESS,
     LOGIN_REQUEST_ERROR,
     LOGIN_REQUEST,
 } from "../actionTypes";
 
-function* loginAsync() {
+function* loginAsync({ payload: { email, password } }) {
+    const model = {
+        application_id: 'a68be319fca51caca60eed5711226e568bd1c1d13ff452b945515f1a6ffbaca4',
+        email,
+        password
+    };
     try {
-        const response = call(getDataAction, config.webApiUrl, false);
-
+        const response = yield call(postDataAction, config.webApiUrl, model);
+        setAuthToken(response);
         yield put({
             type: LOGIN_REQUEST_SUCCESS,
-            response
         });
     } catch (error) {
         yield put({
             type: LOGIN_REQUEST_ERROR,
-            error
+            error: {
+                message: error.statusText,
+                status: error.status
+            }
         });
     }
 }
 
-export function* watchLoginAsync() {
+export default function* watchLoginAsync() {
     yield takeEvery(LOGIN_REQUEST, loginAsync)
 }
-
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 const appConfig = {
-    webApiUrl: 'http://35.204.36.69:8001/api/session/create'
+    webApiUrl: 'http://35.204.36.69:8001/api/v1/session/create'
 };
 
 export default appConfig;
